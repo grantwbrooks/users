79efbd604226c8327e226da2ea8ab47310692cbe
grantwbrooks <grantwbrooks@gmail.com>
2017-07-26 20:28:53 -0400

    users project done

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $("form").submit(function(){
+        var first = $("#first_name").val();
+        var last = $("#last_name").val();
+        var email = $("#email").val();
+        var contact = $("#number").val();
+        $("#body").append("<tr><td>"+first+"</td><td>"+last+"</td><td>"+email+"</td><td>"+contact+"</td></tr>");
+        // below code clears value from input boxes
+        $("#first_name").val("");
+        $("#last_name").val("");
+        $("#email").val("");
+        $("#number").val("");
+        return false;
+    })
+
+});
