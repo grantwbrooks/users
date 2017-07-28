1c9ba7953c45d1c0ca5ed4a46457664ec16037e9
grantwbrooks <grantwbrooks@gmail.com>
2017-07-27 20:19:06 -0400

    finished weather

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         var last = $("#last_name").val();
         var email = $("#email").val();
         var contact = $("#number").val();
-        $("#body").append("<tr><td>"+first+"</td><td>"+last+"</td><td>"+email+"</td><td>"+contact+"</td></tr>");
+        $("#tablebody").append("<tr><td>"+first+"</td><td>"+last+"</td><td>"+email+"</td><td>"+contact+"</td></tr>");
         // below code clears value from input boxes
         $("#first_name").val("");
         $("#last_name").val("");
